71b203f9511078e06ce6056100b19069d1cfc8f0
groberto27 <151818633+groberto27@users.noreply.github.com>
2023-12-10 07:39:06 -0500

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -35,7 +35,7 @@ lr.fit(X,y)
 y_pred = lr.predict(X)
 
 # Income
-Income = st.selectbox("Income", 
+income = st.selectbox("Income", 
              options = ["Less than $10,000",
                        "10 to under $20,000",
                         "20 to under $30,000",
@@ -45,7 +45,7 @@ Income = st.selectbox("Income",
                         "75 to under $100,000",
                         "100 to under $150,000",
                         "$150,000 +"])
-st.write(f"Income (pre-conversion): {Income}")
+st.write(f"Income (pre-conversion): {income}")
 if income == "Less than $10,000":
     income = 1
 elif income == "10 to under $20,000":
@@ -64,7 +64,7 @@ elif income == "100 to under $150,000":
     income = 8    
 else:
     income  = 9 
-st.write(f"Income (post-conversion): {Income}")
+st.write(f"Income (post-conversion): {income}")
 
 # Education
 education = st.selectbox("Education level", 
@@ -76,7 +76,7 @@ education = st.selectbox("Education level",
                         "Four-year college/ Bachelors",
                         "Some post-graduate or professional schooling",
                         "Graduate or professional degree (MA, MS, Phd, MD, JD, others"])
-
+st.write(f"Income (pre-conversion): {education}")
 if education == "Less than high school":
     education = 1
 elif education == "Some high high school":
@@ -93,6 +93,7 @@ elif education == "Some post-graduate or professional schooling":
     education = 7
 else:
     education = 8
+st.write(f"Income (post-conversion): {education}")
 
 marital=st.number_input('What is your current marital status',1,6)
 parent=st.slider('Do you have children',0,1)
