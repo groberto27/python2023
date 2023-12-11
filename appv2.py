57c0f2c46e906cbb9cd34fe2d39ca54aec8df8a0
groberto27 <151818633+groberto27@users.noreply.github.com>
2023-12-10 21:09:00 -0500

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LogisticRegression
 
 header=st.container()
 features=st.container()
-st.image('li.jpg',caption="Image Source: LinkedIn",width=700)
+st.image('li.jpg',caption="Image Source: LinkedIn",width=500)
 
 st.markdown(
     """
