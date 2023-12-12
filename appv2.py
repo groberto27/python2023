7b821f4a4d2eacbddde0235835b3b98fa04b7ed0
groberto27 <151818633+groberto27@users.noreply.github.com>
2023-12-12 00:08:35 -0500

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -164,7 +164,7 @@ age = st.number_input(label="What is your age?",
            max_value=98)
 
 #Making predictions 
-if st.button ('Make Prediction'):
+with st.button ('Make Prediction'):
     person =  np.array([income,education,marital,parent,gender,age])
     predicted_class = lr.predict([person])
     probs = lr.predict_proba([person])
