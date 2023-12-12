f0f07299e771f68ae625bc88f8e0fe301a44bc3e
groberto27 <151818633+groberto27@users.noreply.github.com>
2023-12-12 00:07:27 -0500

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -168,6 +168,8 @@ if st.button ('Make Prediction'):
     person =  np.array([income,education,marital,parent,gender,age])
     predicted_class = lr.predict([person])
     probs = lr.predict_proba([person])
-    st.write(f**"Predicted class: {predicted_class[0]} # 0=Doesn't use LinkedIn, 1=Uses LinkedIN"**) 
-    st.write(f**"The probability that you're a  LinkedIn user is: {probs[0][1]}"**)
+
+    
+    #st.write(f"Predicted class: {predicted_class[0]} 0=Doesn't use LinkedIn, 1=Uses LinkedIN") 
+    #st.write(f**"The probability that you're a  LinkedIn user is: {probs[0][1]}"**)
 
